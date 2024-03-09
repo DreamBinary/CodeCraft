7444f31b763698fd015144a5f96f105b785144cb
fiv <1647715359@qq.com>
2024-03-09 20:03:43 +0800

    update

--- run.sh
+++ run.sh
@@ -2,14 +2,22 @@
 
 c++ main.cpp -o main
 
-if ! read -t 2 -p "Path of map: " map_path; then
+if [ -n "$1" ]; then
+    map_path=$1
+    map_name=$(basename $map_path)
+else
     paths=$(ls "./maps" | sort)
     paths_array=($paths)
+    map_name=${paths_array[-1]}
     map_path="./maps/${paths_array[-1]}"
 fi
 
-./PreliminaryJudge ./main -m $map_path
+log_path="./logs/${map_name}_$(date +%Y%m%d%H%M%S).log"
+
+
+./PreliminaryJudge ./main -m $map_path >> ${log_path}
 
 ./replayer/CodeCraft_2024_Replay.x86_64
 
 wait
+
